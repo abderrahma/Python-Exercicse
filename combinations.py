c8272fbfe62a60e985317dc381b2aaf31687566a
abderrahma <abde.james47@gmail.com>
2026-01-29 17:42:37 +0100

    Using the math library

--- combinations.py
+++ combinations.py
@@ -1,7 +1,4 @@
-def fact(n):
-    if n == 0 or n == 1:
-        return 1
-    return n * fact(n - 1)
+import math
 
 def comb(n, k):
-    return fact(n) // (fact(k) * fact(n - k))
+    return math.comb(n, k)
